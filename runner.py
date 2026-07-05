9d7ab3a9a7d4afcd028123893c697cacc9ba92f7
EH225 <eric.helmold@gmail.com>
2026-07-05 16:04:50 -0400

    update runner with run_pretraining

--- runner.py
+++ runner.py
@@ -13,6 +13,26 @@ from dataset_utils import get_dataloader
 from trainer import Trainer
 
 
+def run_pretraining(config_name: str, dataset_dir: str) -> None:
+    """
+    This helper function runs pre-training of the model for a given config file specified by
+    config_name. This function:
+        1. Reads in the config file as a dict
+        2. Builds the train and val dataloaders
+        3. Constructs the Trainer obj
+        4. Run the training loop by calling trainer.pretrain()
+
+    :param config_name: The name of the config file to use for running training.
+    :param dataset_dir: The directory location of the dataset.
+    :return: None.
+    """
+    config = read_yaml(os.path.join(CURRENT_DIR, "config", f"{config_name}.yml"))
+    dataloaders = {"train": get_dataloader(dataset_dir, "train", 5),
+                   "val": get_dataloader(dataset_dir, "val", 5)}
+    trainer = Trainer(config=config, dataloaders=dataloaders)
+    trainer.pretrain()
+
+
 def run_training(config_name: str, dataset_dir: str) -> None:
     """
     This helper function runs training of the model for a given config file specified by
